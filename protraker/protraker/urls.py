7ab65924dcb9f0e6df5a8b5767ef859fb94568b4
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2021-03-07 22:56:50 +0300

    Сделал login u logout для пользователей

--- protraker/protraker/urls.py
+++ protraker/protraker/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from todostuff.views import todoappView, addTodo, deleteTodo
 from users.views import register 
@@ -25,4 +26,6 @@ urlpatterns = [
     path('addTodo/', addTodo),
     path('deleteTodoItem/<int:item>/', deleteTodo),
     path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
