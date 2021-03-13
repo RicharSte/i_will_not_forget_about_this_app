98d616694095e7b7e41f0e7fc6f7cb85a65daa3c
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2021-03-13 20:26:35 +0300

    Теперь каждый автор видит только свою запись, добавлен быстрый доступ к админке

--- protraker/todostuff/models.py
+++ protraker/todostuff/models.py
@@ -1,4 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Todo(models.Model):
     content = models.TextField()
+    
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    
--- protraker/todostuff/views.py
+++ protraker/todostuff/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 
 from .models import Todo
 
 def todoappView(request):
-    all_todo_items = Todo.objects.all()
+    all_todo_items = Todo.objects.filter(autor_id = request.user)
     return render(request, 'todo/home.html', {'all_items':all_todo_items, 'title': 'Add some tasks'})
 
 def addTodo(request):
     text = request.POST['content']
     new_todo = Todo(content = text)
+    new_todo.autor = request.user
     new_todo.save()
-    return HttpResponseRedirect('/todoapp/')
+    return redirect ('todo-home')
 
 def deleteTodo(request, item):
     delete_item = Todo.objects.get(id= item)
     delete_item.delete()
-    return HttpResponseRedirect('/todoapp/')
+    return redirect ('todo-home')
 
