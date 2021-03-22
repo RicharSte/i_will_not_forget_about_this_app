b51d6ee23be58b91a3d9b908125eba4d0fb65e29
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2021-03-22 22:37:43 +0300

    Все сделал, осталось навести порядок во фронте и задеплоить

--- protraker/statistic/views.py
+++ protraker/statistic/views.py
@@ -3,18 +3,40 @@ from django.contrib.auth.decorators import login_required
 
 from todostuff.models import Todo
 
-from datetime import date
+from datetime import date, timedelta
 
 @login_required(login_url='login')
 def day_statistic(request):
     undone_today = len(Todo.objects.filter(autor_id = request.user, done = 0, done_date = date.today()))
     done_today = len(Todo.objects.filter(autor_id = request.user, done = 1, done_date = date.today()))
+   
+    week_work = done_in_one_period(request, date.today()-timedelta(days=7), date.today())
+    month = done_in_one_period(request, date.today()-timedelta(days=30), date.today())
     
-    done_in_percentage = "{:.2f}".format((done_today/(done_today + undone_today)) * 100)
+    if done_today + undone_today != 0:
+        done_in_percentage = "{:.2f}".format((done_today/(done_today + undone_today)) * 100)
+        
+        context = {
+            'undone_today':undone_today,
+            'done_in_percentage': done_in_percentage,
+            'week_work':week_work,
+            'month':month
+        }
+        
+        return render(request, 'statistic/home.html', context)
+    else:
+        text = '0'
+        return render(request, 'statistic/home.html', {'done_in_percentage': text})
     
-    context = {
-        'undone_today':undone_today,
-        'done_in_percentage': done_in_percentage
-    }
+
+def daterange(date1, date2):
+    for n in range(int ((date2-date1).days)+1):
+        yield date1 + timedelta(n)
+        
+def done_in_one_period(request, date1, date2):
+    tasks = 0
+    for date in daterange(date1, date2):
+        tasks += len(Todo.objects.filter(autor_id = request.user, done = 1, done_date = date))
+        
+    return tasks
     
-    return render(request, 'statistic/home.html', context)
